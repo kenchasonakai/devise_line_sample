3a925f822584644f30fc4ff46eab6dbe0fa2f521
kenchasonakai <kenchasonakai@gmail.com>
2024-10-04 15:13:00 +0900

    add: auth0の鍵情報を登録

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :auth0, Rails.application.credentials.dig(:omniauth, :auth0, :client_id), Rails.application.credentials.dig(:omniauth, :auth0, :client_secret), Rails.application.credentials.dig(:omniauth, :auth0, :domain), scope: 'openid profile'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
